39338e7a2a5e323da9a311a5a88cd68e63b1f57e
rmarfatia <rmarfatia@pinterest.com>
2021-04-04 15:32:46 -0700

    Adding some colors

--- app/src/main/java/me/rikinmarfatia/hail/MainActivity.kt
+++ app/src/main/java/me/rikinmarfatia/hail/MainActivity.kt
@@ -3,28 +3,37 @@ package me.rikinmarfatia.hail
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
 import me.rikinmarfatia.hail.ui.theme.HailTheme
+import me.rikinmarfatia.hail.ui.theme.backgroundBlue
+import me.rikinmarfatia.hail.ui.theme.cellBlue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HailTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = backgroundBlue) {
                     WeatherFeed()
                 }
             }
@@ -34,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherFeed() {
-    val viewModel: WeatherViewModel = mavericksViewModel()
+    val viewModel = remember { WeatherViewModel() }
     val state by viewModel.collectAsState { it.feed }
 
     WeatherFeed(state)
@@ -54,8 +63,11 @@ fun WeatherRow(state: WeatherState) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly
+            .height(80.dp)
+            .padding(horizontal = 8.dp)
+            .background(color = cellBlue, shape = RoundedCornerShape(size = 8.dp)),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(state.date)
         Text(state.low.toString())
@@ -63,11 +75,11 @@ fun WeatherRow(state: WeatherState) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun WeatherFeedPreview() {
     HailTheme(darkTheme = true) {
-        Surface {
+        Surface(color = backgroundBlue) {
             val viewModel = remember {
                 WeatherViewModel(
                     weatherRepository = WeatherRepository(live = FakeWeatherRepository())
