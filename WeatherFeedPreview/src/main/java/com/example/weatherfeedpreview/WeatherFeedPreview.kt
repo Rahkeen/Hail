5841bae7205b48b7f0457523b7f2e521a8a0d3ac
rmarfatia <rmarfatia@pinterest.com>
2021-04-20 22:47:28 -0700

    Added More Weather Repository Fakes

--- WeatherFeedPreview/src/main/java/com/example/weatherfeedpreview/WeatherFeedPreview.kt
+++ WeatherFeedPreview/src/main/java/com/example/weatherfeedpreview/WeatherFeedPreview.kt
@@ -5,8 +5,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherfeedfeature.WeatherFeed
-import com.example.weatherrepo.FakeWeatherRepository
 import com.example.weatherfeedfeature.WeatherViewModel
+import com.example.weatherrepo.FakeWeatherRepository
 import me.rikinmarfatia.hail.ui.theme.HailTheme
 
 @Preview
