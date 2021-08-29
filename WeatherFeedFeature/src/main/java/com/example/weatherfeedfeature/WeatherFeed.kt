32c1905070e499bcee2c8ba2f4007c6ee9b341c1
rmarfatia <rmarfatia@pinterest.com>
2021-08-29 01:01:38 -0700

    Update Compose + Dependencies

--- WeatherFeedFeature/src/main/java/com/example/weatherfeedfeature/WeatherFeed.kt
+++ WeatherFeedFeature/src/main/java/com/example/weatherfeedfeature/WeatherFeed.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,24 +21,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import me.rikinmarfatia.hail.features.weather.presentation.WeatherFeedState
 import me.rikinmarfatia.hail.features.weather.presentation.WeatherState
 import me.rikinmarfatia.hail.ui.theme.backgroundBlue
 import me.rikinmarfatia.hail.ui.theme.cellBlue
 
 @Composable
-fun WeatherFeed(weatherViewModel: WeatherViewModel, navController: NavController) {
+fun WeatherFeed(weatherViewModel: WeatherViewModel, navigate: (String) -> Unit) {
     val state by weatherViewModel.feedStates().collectAsState()
 
     WeatherFeed(
         state = state,
         action = weatherViewModel::feedItemSelected,
-        navigate = navController::navigate
+        navigate = navigate
     )
 }
 
--- app/src/main/java/me/rikinmarfatia/hail/MainActivity.kt
+++ app/src/main/java/me/rikinmarfatia/hail/MainActivity.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherfeedfeature.WeatherFeed
-import com.example.weathermetadatafeature.WeatherMetadata
-import com.example.weatherrepo.FakeWeatherRepository
-import com.example.weatherrepolive.RealWeatherRepository
-import me.rikinmarfatia.hail.features.weather.presentation.WeatherFeedState
 import com.example.weatherfeedfeature.WeatherViewModel
 import com.example.weatherfeedfeature.WeatherViewModelFactory
+import com.example.weathermetadatafeature.WeatherMetadata
+import com.example.weatherrepolive.RealWeatherRepository
+import me.rikinmarfatia.hail.features.weather.presentation.WeatherFeedState
 import me.rikinmarfatia.hail.ui.theme.HailTheme
 
 @ExperimentalAnimationApi
@@ -33,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     )
                 )
                 NavHost(navController = navController, startDestination = "feed") {
-                    composable("feed") { WeatherFeed(weatherViewModel, navController) }
+                    composable("feed") { WeatherFeed(weatherViewModel, navController::navigate) }
                     composable("metadata") {
                         WeatherMetadata(weatherViewModel)
                     }
